0de4643d3895e49730d1e21dcde3ecf1b4b703ee
Ming Li <Mingrkli@gmail.com>
2022-08-01 20:46:10 -0700

    Update page-travel.js

--- js/page-travel.js
+++ js/page-travel.js
@@ -2,13 +2,13 @@ const navHomeBtn = document.getElementById('nav-home-btn');
 const navProductsBtn = document.getElementById('nav-products-btn');
 
 navHomeBtn.onclick = () => {
-    if (location.href != "http://127.0.0.1:5500/my_random_experiments/mesco/index.html") {
-        location.href = "http://127.0.0.1:5500/my_random_experiments/mesco/index.html";
+    if (location.href != "index.html") {
+        location.href = "index.html";
     }
 }
 
 navProductsBtn.onclick = () => {
-    if (location.href != "http://127.0.0.1:5500/my_random_experiments/mesco/searching.html") {
-        location.href = "http://127.0.0.1:5500/my_random_experiments/mesco/searching.html";
+    if (location.href != "searching.html") {
+        location.href = "searching.html";
     }
 }
